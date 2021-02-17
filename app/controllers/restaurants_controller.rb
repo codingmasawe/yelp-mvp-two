342d1bf6280d2df8ebcae5829cb9bc2ff9c1194e
Nikolas Wünnenberg <n.wuennenberg@gmail.com>
2021-02-17 21:36:14 +0100

    new

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,20 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "123213", category: "chinese" }
+pizza_east =  { name: "Pizza East", address: "4738  Snider Street", phone_number: "17873213", category: "belgian" }
+pizza_south =  { name: "Pizza South", address: "869  Eagle Street", phone_number: "12893213", category: "french" }
+pizza_north =  { name: "Pizza North", address: "1135  White Avenue", phone_number: "12390213", category: "japanese" }
+pizza_west =  { name: "Pizza West", address: "2614  South Street", phone_number: "12378713", category: "italian" }
+
+[ dishoom, pizza_east, pizza_south, pizza_north, pizza_west ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+
+
+
